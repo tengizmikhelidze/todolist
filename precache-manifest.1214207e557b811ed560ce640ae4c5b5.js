e3cbbc52fde23aa5f9eaa14d413d9b1daa84070e
Tengiz Mikhelidze <mixelidzetengizi@gmail.com>
2020-10-03 15:46:53 +0400

    Updates

---
+++ precache-manifest.1214207e557b811ed560ce640ae4c5b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfd92d5afcb16cc969d51cb58db7fd1",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "13a61e48b93ecf47d5a8",
+    "url": "/todolist/static/css/main.4e32b4d8.chunk.css"
+  },
+  {
+    "revision": "0136529369f6865c379e",
+    "url": "/todolist/static/js/2.f3d11356.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.f3d11356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a61e48b93ecf47d5a8",
+    "url": "/todolist/static/js/main.2d2d2fe7.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
